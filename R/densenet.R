f944e36f6f0d0c97a929668f64f4f0abecf7711f
Daniel Falbel <dfalbel@gmail.com>
2017-08-02 14:03:20 -0300

    add denseblock

--- R/densenet.R
+++ R/densenet.R
@@ -78,3 +78,35 @@ transition <- function(x, nb_filter, dropout_rate = NULL, weight_decay = 1e-4) {
       strides = c(2,2)
     )
 }
+
+#' Build a denseblock where the output of each conv_factory is fed to
+#' subsequent ones
+#'
+denseblock <- function(x, nb_layers, nb_filter, growth_rate) {
+
+  list_feat <- list(x)
+
+  if (keras::backend()$image_dim_ordering() == "th") {
+    concat_axis = 1
+  } else if (keras::backend()$image_dim_ordering == "tf"){
+    concat_axis <- -1
+  }
+
+  for (i in 1:nb_layers) {
+
+    x <- conv_factory(x, growth_rate, dropout_rate, weight_decay)
+    list_feat[[length(list_feat) + 1]] <- x
+    x <- keras::layer_concatenate(list_feat, concat_axis = concat_axis)
+    nb_filter <- nb_filter + growth_rate
+
+  }
+
+
+  list(
+    x = x,
+    nb_filter = nb_filter
+  )
+}
+
+
+
