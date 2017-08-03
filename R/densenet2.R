9ecd1228e41908474b173467e48607ffc23319c0
Daniel Falbel <dfalbel@gmail.com>
2017-08-03 18:45:40 -0300

    docs denseblock

--- R/densenet2.R
+++ R/densenet2.R
@@ -73,3 +73,68 @@ conv_block <- function(ip, nb_filter,
 }
 
 
+#' Apply BatchNorm, Relu 1x1, Conv2D, optional compression, dropout and Maxpooling2D
+#'
+#' @param ip: Input keras tensor
+#' @param nb_filter: number of filters
+#' @param compression: calculated as 1 - reduction. Reduces the number of
+#'                     feature maps in the transition block.
+#' @param dropout_rate: dropout rate
+#' @param weight_decay: weight decay factor
+#'
+transition_block <- function(ip, nb_filter,
+                             compression = 1,
+                             dropout_rate = NULL,
+                             weight_decay = 1e-4) {
+
+  if (keras::backend()$image_data_format() == "channels_first") {
+    concat_axis <- 1
+  } else {
+    concat_axis <- -1
+  }
+
+  x <- ip %>%
+    keras::layer_batch_normalization(
+      axis = concat_axis,
+      gamma_regularizer = keras::regularizer_l2(weight_decay),
+      beta_regularizer = keras::regularizer_l2(weight_decay)
+    ) %>%
+    keras::layer_activation(activation = "relu") %>%
+    keras::layer_conv_2d(
+      filters = rep(trunc(nb_filter * compression)),
+      kernel_size = c(1,1),
+      kernel_initializer = "he_uniform",
+      padding = "same",
+      use_bias = FALSE,
+      kernel_regularizer = keras::regularizer_l2(weight_decay)
+    )
+
+  if(!is.null(dropout_rate)){
+    x <- keras::layer_dropout(x, rate = dropout_rate)
+  }
+
+
+  x %>%
+    keras::layer_average_pooling_2d(
+      pool_size = c(2,2),
+      strides = c(2,2)
+    )
+}
+
+#' Build a dense_block where the output of each conv_block is fed to subsequent
+#' ones
+#'
+#'
+#' @param x: keras tensor
+#' @param nb_layers: the number of layers of conv_block to append to the model.
+#' @param nb_filter: number of filters
+#' @param growth_rate: growth rate
+#' @param bottleneck: bottleneck block
+#' @param dropout_rate: dropout rate
+#' @param weight_decay: weight decay factor
+#' @param grow_nb_filters: flag to decide to allow number of filters to grow
+#' @param return_concat_list: return the list of feature maps along with the actual output
+dense_block <- function() {
+
+}
+
